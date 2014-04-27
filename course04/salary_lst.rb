4a3ee154ccb7235e5b2b637ec15cc844ec610445
poc7667 <poc7667@gmail.com>
2014-04-27 18:40:10 +0800

    search engine
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- course04/salary_lst.rb
+++ course04/salary_lst.rb
@@ -5,7 +5,6 @@ require 'pry'
 require 'action_view'
 include ActionView::Helpers
 
-#read the excel filename
 excel_filename= ARGV[0]
 #read Excel
 input = Roo::Excel.new(excel_filename)
@@ -16,22 +15,17 @@ input.default_sheet = input.sheets[0]
 
 (input.first_row+1).upto(input.last_row) do |line_no|
   this_row = input.row(line_no)
-  salary_lst << this_row
+  name = this_row[1].downcase + " " + this_row[2].downcase
+  summary[name] = [this_row.first] + this_row[4..-1]
 end
 
-lo_to_hi = salary_lst.sort_by{|k|k.last}
-hi_to_lo = lo_to_hi.reverse
-
-one_tenth, one_fifth = lo_to_hi.count/10, lo_to_hi.count/5
-
-sum = 0
-0.upto(one_fifth-1) do |line_no|
-  sum += lo_to_hi[line_no].last
+while c = $stdin.gets.downcase.chomp and c != 'q'
+  next if 0 == c.strip.length   
+  result = summary.keys.grep(/#{c}/)
+  ap("Total: #{result.count}")
+  result.each do |key|
+    p key
+    ap([summary[key].first, summary[key].last])
+  end
 end
-ap("bottom #{one_fifth} sum:#{sum}, average #{sum/one_fifth}")
-
-sum = 0.upto(one_tenth-1).inject(0) do |sum,i|
-  len = (lo_to_hi.count-1)
-  sum + lo_to_hi[ len-i ].last
-end
-ap("top #{one_tenth} sum:#{(sum)}, average #{sum/one_tenth}")
+p "bye~bye~"
