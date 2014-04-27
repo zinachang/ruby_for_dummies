81934c917abd60326f16ac7b9bd680e58fa1fc62
poc7667 <poc7667@gmail.com>
2014-04-27 17:26:44 +0800

    With  Hash  and ActionView
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- course04/salary_lst.rb
+++ course04/salary_lst.rb
@@ -1,30 +1,28 @@
 # encoding: utf-8
 require 'roo'
 require 'awesome_print'
-require 'fileutils'
 require 'pry'
+require 'action_view'
+include ActionView::Helpers
 
 excel_filename= ARGV[0]
+#read Excel
 input = Roo::Excel.new(excel_filename)
 salary_lst = []
-
 output = Array.new
 summary = Hash.new
 input.default_sheet = input.sheets[0]
 
-
 (input.first_row+1).upto(input.last_row) do |line_no|
-  salary_lst << input.row(line_no)
+  this_row = input.row(line_no)
+  salary_lst << this_row
+  summary[this_row[1, 2].map(&:downcase).join(' ')] = [this_row.first] + this_row[4..-1]
 end
 
 lo_to_hi = salary_lst.sort_by{|k|k.last}
+hi_to_lo = lo_to_hi.reverse
 
-high_to_low_salary = salary_lst
-                     .sort_by{|k|k.last}
-                     .reverse
-n_lines = (input.last_row - input.first_row + 1).to_i
-one_tenth = (n_lines) / 10
-one_fifth = (n_lines) / 5
+one_tenth, one_fifth = lo_to_hi.count/10, lo_to_hi.count/5
 
 sum = 0
 0.upto(one_fifth-1) do |line_no|
@@ -32,14 +30,19 @@ sum = 0
 end
 ap("bottom #{one_fifth} sum:#{sum}, average #{sum/one_fifth}")
 
-sum = 0
-0.upto(one_tenth-1) do |line_no|
-  # begin
-    sum += lo_to_hi[lo_to_hi.count-line_no].last
-  # rescue Exception => e
-  #   ap(lo_to_hi.first)  
-  #   ap(lo_to_hi.last)  
-  #   ap(lo_to_hi[high_to_low_salary.count-line_no])  
-  # end
+sum = 0.upto(one_tenth-1).inject(0) do |sum,i|
+  sum + lo_to_hi[ (lo_to_hi.count-1) -i ].last
 end
-ap("top #{one_tenth} sum:#{sum}, average #{sum/one_tenth}")
+
+ap("top #{one_tenth} sum:#{(sum)}, average #{sum/one_tenth}")
+
+while c = $stdin.gets.downcase.chomp and c != 'q'
+  next if 0 == c.strip.length 
+  
+  result = summary.keys.grep(/#{c}/)
+  ap("Total: #{result.count}")
+  result.each do |key|
+    p key
+    ap([summary[key].first, summary[key].last])
+  end
+end
