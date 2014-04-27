c8f2c4e2186c7032b84309cf1f2650ff68e6958b
poc7667 <poc7667@gmail.com>
2014-04-27 17:37:26 +0800

    Remove Hash
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- course04/salary_lst.rb
+++ course04/salary_lst.rb
@@ -5,6 +5,7 @@ require 'pry'
 require 'action_view'
 include ActionView::Helpers
 
+#read the excel filename
 excel_filename= ARGV[0]
 #read Excel
 input = Roo::Excel.new(excel_filename)
@@ -16,7 +17,6 @@ input.default_sheet = input.sheets[0]
 (input.first_row+1).upto(input.last_row) do |line_no|
   this_row = input.row(line_no)
   salary_lst << this_row
-  summary[this_row[1, 2].map(&:downcase).join(' ')] = [this_row.first] + this_row[4..-1]
 end
 
 lo_to_hi = salary_lst.sort_by{|k|k.last}
@@ -31,18 +31,7 @@ end
 ap("bottom #{one_fifth} sum:#{sum}, average #{sum/one_fifth}")
 
 sum = 0.upto(one_tenth-1).inject(0) do |sum,i|
-  sum + lo_to_hi[ (lo_to_hi.count-1) -i ].last
+  len = (lo_to_hi.count-1)
+  sum + lo_to_hi[ len-i ].last
 end
-
 ap("top #{one_tenth} sum:#{(sum)}, average #{sum/one_tenth}")
-
-while c = $stdin.gets.downcase.chomp and c != 'q'
-  next if 0 == c.strip.length 
-  
-  result = summary.keys.grep(/#{c}/)
-  ap("Total: #{result.count}")
-  result.each do |key|
-    p key
-    ap([summary[key].first, summary[key].last])
-  end
-end
